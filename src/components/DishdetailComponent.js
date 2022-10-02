d3170c46eda44756ca6c4b0b37ef4af56cc48d13
kobaf <andre@dorax-invest.com>
2022-10-02 18:40:48 +0300

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderComments({ comments }) {
         if (comments != null) {
@@ -28,17 +29,27 @@ import { Card, CardImg, CardText, CardBody,
 
     const DishDetail = (props) => {
         return (
-            <div className='container'>
-                <div className="row">
-                    <div  className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
-                    </div>
-                    <div  className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.dish !== undefined ? props.dish.comments : null} /> 
-                    </div>
+            <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
-        )
+            </div>
+        );
     }
 
     function RenderDish({ dish }) {
@@ -56,6 +67,6 @@ import { Card, CardImg, CardText, CardBody,
             else {
                 return <div></div>
             }
-    }
+        }
 
-    export default DishDetail;
+export default DishDetail;
